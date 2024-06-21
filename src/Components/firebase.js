ae171c7229b7c225063afa70973e71ff553e74e5
Manan-Affinsys <manan.dudhwala@affinsys.com>
2024-06-21 12:25:48 +0530

    dummy commit

--- src/Components/firebase.js
+++ src/Components/firebase.js
@@ -2,7 +2,6 @@ import { initializeApp } from "firebase/app";
 import { getAuth, signInWithPopup, signOut } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 
-console.log(import.meta.env.VITE_FIREBASE_APP_KEY)
 
 
 const firebaseConfig = {
